1bf3bf0c4d0d4aa9d441276b8db40e73fe81a064
brunsilva-jc <brunsilva.jc@gmail.com>
2022-07-19 14:26:35 -0300

    following the links

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -15,3 +15,8 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').get()
             }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
